ccd6729596192de000f4975c2718ec20287ccd64
Yukinoshita <53087601+FujiwaraChika0303@users.noreply.github.com>
2024-02-16 09:35:55 +0800

    Update AllBox.vue

--- src/components/AllFunc/AllBox.vue
+++ src/components/AllFunc/AllBox.vue
@@ -3,7 +3,9 @@
     <n-tab-pane class="no-padding" name="link" tab="捷径">
       <ShortCut />
     </n-tab-pane>
-    <n-tab-pane name="note" tab="便签"> 即将完善 </n-tab-pane>
+    <n-tab-pane class="no-padding" name="Music" tab="音乐">
+      <Music />
+    </n-tab-pane>
     <n-tab-pane name="more" tab="待办"> 还能有啥呢 😢 </n-tab-pane>
   </n-tabs>
 </template>
